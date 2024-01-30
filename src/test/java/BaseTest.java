c859cb4caf1a6d8b9574d999a36cb593b5c35b4a
theythemclemence <157631845+theythemclemence@users.noreply.github.com>
2024-01-29 21:42:18 -0800

    Uodated BaseTest
    Updated BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,16 +4,58 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class BaseTest {
-
+    public WebDriver driver = null;
+    public String url = "https://qa.koel.app/";
     @BeforeSuite
-    static void setupClass() {
+    void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
+    @BeforeMethod
+    public void launchBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
 
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
 }
+
+@AfterMethod
+public void closeBrowser() {
+driver.quit();
+}
+
+@Test
+public void loginEmptyEmailPassword(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
+}
+@Test
+public void loginValidEmailPassword(){
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+
+    WebDriver driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+}
+
+
