b2a808fcd95507f59c173aa075fe69259526f9ad
theythemclemence <157631845+theythemclemence@users.noreply.github.com>
2024-01-29 16:28:39 -0800

    trying to clean this up
    trying to clean this up

---
+++ src/test/java/Homework16_revision.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16_revision extends LoginTests {
+
+    @Test
+        public void registrationNavigation() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        
+
+    }
+
