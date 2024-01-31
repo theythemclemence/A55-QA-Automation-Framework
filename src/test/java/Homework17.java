e0758ea63441e048a8272369736b8a2ffde2a7dc
theythemclemence <157631845+theythemclemence@users.noreply.github.com>
2024-01-31 12:19:47 -0800

    HW-17 Resubmission
    HW-17 Resubmission

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -13,58 +13,41 @@ public class Homework17 extends BaseTest {
     @Test
     public void addSongToPlaylist() throws InterruptedException {
 
-        String actual_text = "Added 1 song into \"Test\"";
-        String expected_text = "Added 1 song into \"Test\"";
+        String expectedSongAddedMessage = "Added 1 song into \"Test Pro Playlist.\"";
 
-        // 1. Navigate to Koel
         navigateToPage();
-
-        // 2. Login with valid credentials
-        Thread.sleep(2000);
         provideEmail("clemence.breslin@testpro.io");
         providePassword("0usTFBYn");
         clickSubmit();
-
-        // 3. Search for a song
         Thread.sleep(2000);
-        WebElement searchField = driver.findElement(By.cssSelector("div#searchForm input[type='search']"));
-        searchField.sendKeys("Dark Days");
-        clickSubmit();
-
-        // 4. Click 'View All' button
-        Thread.sleep(2000);
-        WebElement viewAllBtn = driver.findElement(By.xpath("//button[@data-test='view-all-songs-btn']"));
-        clickSubmit();
-
-        // 5. Click on first result
-        Thread.sleep(2000);
-        WebElement firstSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
-        clickSubmit();
-
-        //6. Click 'Add To' button
-        Thread.sleep(2000);
-        WebElement addToBtn = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@data-test='add-to-btn']"));
-        clickSubmit();
-
-        //7. Add song to existing playlist
+        searchSong("Dark Days");
+        clickViewAllBtn();
+        selectFirstSongResult();
+        clickAddToBtn();
         Thread.sleep(2000);
         choosePlaylist();
-
-        //8. Verify that notification appears confirming song was added to Playlist
         Thread.sleep(2000);
-        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
-
-        Assert.assertEquals(actual_text, expected_text);
-
-        driver.quit();
-
-
+        Assert.assertEquals(getAddToPlaylistSuccessMsg(), expectedSongAddedMessage);
     }
 
     // Helper Methods
 
+    public void clickViewAllBtn() throws InterruptedException {
+        WebElement viewAll = driver.findElement(By.xpath("//button[@data-test='view-all-songs-btn']"));
+        viewAll.click();
+        Thread.sleep(2000);
+    }
+    public void selectFirstSongResult() throws InterruptedException {
+        WebElement firstSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
+        firstSong.click();
+        Thread.sleep(2000);
+    }
+    public String getAddToPlaylistSuccessMsg() {
+        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+        return notification.getText();
+    }
     public void choosePlaylist() throws InterruptedException {
-        WebElement playlist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'Test Playlist')]"));
+        WebElement playlist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'Test Pro Playlist')]"));
         playlist.click();
         Thread.sleep(2000);
     }
@@ -74,10 +57,9 @@ public class Homework17 extends BaseTest {
         searchField.sendKeys(name);
         Thread.sleep(2000);
     }
-
     public void clickAddToBtn() {
-        WebElement addToBtn = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@data-test='add-to-btn']"));
-        addToBtn.click();
+        WebElement addToButton = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@data-test='add-to-btn']"));
+        addToButton.click();
 
+        }
     }
-}
