bd10dd7298d2388a8a88b2f53f1b69f951cd740e
theythemclemence <157631845+theythemclemence@users.noreply.github.com>
2024-01-31 19:23:48 -0800

    HW-18
    HW-18

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -5,28 +5,25 @@ import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest {
     @Test
-    public void playSong() {
+    public void playSong() throws InterruptedException {
         navigateToPage();
         provideEmail("clemence.breslin@testpro.io");
         providePassword("0usTFBYn");
         clickSubmit();
-        WebElement PlayNext = driver.findElement(By.xpath("//button[@data-test='play-btn']"));
-        clickSubmit();
-        Assert.assertEquals();
-
-
-
-
-        public void verifyPause() {
-            WebElement pauseButton = driver.findElement(By.xpath("//button[@data-test='pause-btn']"));
-        }
-
-
-
-
-
-
-        }
-
-
+        clickPlay();
+        Thread.sleep(2000);
+        Assert.assertTrue(isSongPlaying());
     }
+
+    public boolean isSongPlaying() {
+        WebElement soundBar = driver.findElement(By.xpath("//div/[data-testid='sound-bar-play']"));
+        return soundBar.isDisplayed();
+    }
+
+    public void clickPlay() {
+        WebElement playNextBtn = driver.findElement(By.xpath("//i[data-testid='play-next-btn]"));
+        WebElement playBtn = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+        playNextBtn.click();
+        playBtn.click();
+    }
+}
