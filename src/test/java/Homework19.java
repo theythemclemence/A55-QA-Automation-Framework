ec5711b724a30772043ca1b31cd1ce6e0be3ac21
theythemclemence <157631845+theythemclemence@users.noreply.github.com>
2024-02-02 12:49:55 -0800

    Work in progress
    Work in progress

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,6 +1,10 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class Homework19 extends BaseTest {
+
+    String expectedPlaylistDeletedMessage = "Deleted Playlist \"Test Pro Playlist.\"";
 
    @Test
     public void deletePlaylist() {
@@ -8,8 +12,36 @@ public class Homework19 extends BaseTest {
     provideEmail("clemence.breslin@testpro.io");
     providePassword("0usTFBYn");
     clickSubmit();
+    clickPlaylist();
+    playlistDelete();
+    deleteConfirmation();
     
+
+
    }
 
+public void clickPlaylist () {
+    WebElement playlist = driver.findElement(By.xpath("//section[@id='playlists']//li[contains(text(),'Test Pro Playlist')]"));
+    playlist.click();
 
+    // "//[@id="playlists"]/ul/li[4]/a/text(), 'Test Pro Playlist']"
+    // "//[@id="playlists"]/ul/li[4]/a/text()]"
+}
+
+public void playlistDelete() {
+       WebElement deleteBtn = driver.findElement(By.xpath("//[@id='playlist-Wrapper']/header/div[3]/span/button[2]"));
+       deleteBtn.click();
+
+       public void deleteConfirmation() {
+           WebElement deleteConf = driver.findElement(By.xpath())
+                   // "//[div[3]/div/div/nav/button[2]]"
+    }
+
+    }
+
+    public String getPlaylistDeletedMsg() {
+        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+        return notification.getText();
+
+}
 }
