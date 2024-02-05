a3c8c6a160507301908fba130427f6428640179d
theythemclemence <157631845+theythemclemence@users.noreply.github.com>
2024-02-05 15:10:53 -0800

    HW 19 work in progress
    HW 19 work in progress

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -6,35 +6,36 @@ import org.testng.annotations.Test;
 public class Homework19 extends BaseTest {
 
 
-   @Test
+    @Test
     public void deletePlaylist() throws InterruptedException {
 
-       String expectedPlaylistDeletedMesg = "Deleted playlist \"Test Pro Playlist.\"";
+        String expectedPlaylistDeletedMesg = "Deleted playlist \"Test Pro Playlist.\"";
 
 
-    provideEmail("clemence.breslin@testpro.io");
-    providePassword("0usTFBYn");
-    clickSubmit();
-    openPlaylist();
-    clickDeletePlaylistBtn();
-    Assert.assertEquals(getDeletedPlaylistMsg(), expectedPlaylistDeletedMesg);
+        provideEmail("clemence.breslin@testpro.io");
+        providePassword("0usTFBYn");
+        clickSubmit();
+        openPlaylist();
+        clickDeletePlaylistBtn();
+        Assert.assertEquals(getDeletedPlaylistMsg(), expectedPlaylistDeletedMesg);
 
-   }
+    }
 
-public void clickDeletePlaylistBtn () {
-    WebElement deletePlaylist = driver.findElement(By.cssSelector(".btn-delete-playlist"));
-    deletePlaylist.click();
+    public void clickDeletePlaylistBtn() {
+        WebElement deletePlaylist = driver.findElement(By.cssSelector(".btn-delete-playlist"));
+        deletePlaylist.click();
 
-}
 
-public void openPlaylist() {
-    WebElement playlistToOpen = driver.findElement(By.cssSelector(".playlist:nth-child(4)"));
-    playlistToOpen.click();
-}
+    }
+
+    public void openPlaylist() {
+        WebElement playlistToOpen = driver.findElement(By.cssSelector(".playlist:nth-child(4)"));
+        playlistToOpen.click();
+    }
 
     public String getDeletedPlaylistMsg() {
         WebElement notificationMsg = driver.findElement(By.cssSelector("div.success.show"));
         return notificationMsg.getText();
 
-}
+    }
 }
