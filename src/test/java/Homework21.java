137b03bb79f7106d8a17f6d807a7274453f07e99
theythemclemence <157631845+theythemclemence@users.noreply.github.com>
2024-02-08 14:52:14 -0800

    Work in progress
    Work in progress

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -1,10 +1,13 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class Homework21 extends BaseTest {
 
@@ -21,8 +24,7 @@ public class Homework21 extends BaseTest {
         doubleClickPlaylist();
         enterNewName();
         Assert.assertEquals(getRenamePlaylistSuccessMsg(), updatedPlaylistSuccessMsg);
-
-
+    }
 
 
 
@@ -32,12 +34,29 @@ public String getRenamePlaylistSuccessMsg(){
         }
 
     public void enterNewName() {
+    WebElement playListInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+    playListInputField.sendKeys(Keys.chord(Keys.CONTROL, "A", Keys.BACK_SPACE ));
+    playListInputField.sendKeys(newPlaylistName);
+    playListInputField.sendKeys(Keys.ENTER);
 
     }
+
+    public void displayAllSongs() {
+            List<WebElement> songList = driver.findElements(By.cssSelector("section#playlistWrapper td.title"));
+            System.out.println("Number of Songs found: "+ countSongs());
+            for (WebElement e: songList){
+                System.out.println(e.getText());
+            }
+        }
 
     public void doubleClickPlaylist() {
+    WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+    actions.doubleClick(playlistElement).perform();
 
-    }
-}
+    public String getPlaylistDetails() {
+                return driver.findElement(By.cssSelector("span.meta.text-secondary span.meta")).getText();
+            }
+
+
 
 
