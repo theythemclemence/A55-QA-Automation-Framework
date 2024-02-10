a4a9305e24ae0dbd316c11c9aff6b7ba93a7cba6
theythemclemence <157631845+theythemclemence@users.noreply.github.com>
2024-02-09 18:30:26 -0800

    Updates
    Updates

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -15,6 +16,7 @@ public class BaseTest {
     public WebDriver driver;
 
     public WebDriverWait wait;
+    public Actions actions;
 
     //public String url = "https://qa.koel.app/";
 
@@ -33,6 +35,7 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //Explicit Wait
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
         driver.manage().window().maximize();
         driver.get(baseURL);
     }
