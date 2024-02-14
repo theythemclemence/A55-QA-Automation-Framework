b9e6ff0a8068fd267e384cd7a7dc64fd51ea43b9
theythemclemence <157631845+theythemclemence@users.noreply.github.com>
2024-02-14 14:40:52 -0800

    Work in progress
    Work in progress

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,12 +3,19 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 
 public class LoginPage extends BasePage {
-    private By submitButtonLocator = By.cssSelector("[type='submit']");
-    private By emailField = By.cssSelector("input[type='email']");
-    private By passwordField = By.cssSelector("input[type='password']");
+
+    @FindBy(css = "[type='submit']")
+    private WebElement submitButtonLocator;
+
+    @FindBy(css = "input[type='email']")
+    private WebElement emailField;
+
+    @FindBy(css = "input[type='password']")
+    private WebElement passwordField; 
 
     public LoginPage(WebDriver givenDriver) {
         super(givenDriver);
