8b3338cd0695bb52af6ff0f949536ef2ad3928d4
theythemclemence <157631845+theythemclemence@users.noreply.github.com>
2024-02-14 15:00:59 -0800

    Work in Progress
    Work in Progress

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -18,24 +19,24 @@ public class BasePage {
     public BasePage(WebDriver givenDriver) {
         driver = givenDriver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        actions = new Actions(driver);
+        PageFactory.initElements(driver, this);
 
     }
 
-    protected void click(By locator) {
-        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    protected void click(WebElement webElement) {
+        wait.until(ExpectedConditions.elementToBeClickable(webElement)).click();
     }
 
-    protected WebElement findElement(By locator) {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    protected WebElement findElement(WebElement webElement) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(webElement));
     }
 
-    protected void contextClick(By locator) {
-        actions.contextClick(findElement(locator)).perform();
+    protected void contextClick(WebElement webElement) {
+        actions.contextClick(findElement(webElement)).perform();
     }
 
-    protected void doubleClick(By locator) {
-        actions.doubleClick(findElement(locator)).perform();
+    protected void doubleClick(WebElement webElement) {
+        actions.doubleClick(findElement(webElement)).perform();
     }
-    
+
 }
